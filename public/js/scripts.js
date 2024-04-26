5979cff54d1d54f472158a50295d86ab5ae1f572
Luc Ravenelle <lrav35@gmail.com>
2024-04-26 13:26:06 -0400

    adding game

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,12 +18,38 @@
 /************************************************************************/
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
+const randNum = Math.floor(Math.random() * 100);
+const guesses = [];
+const validateInput = (input) => {
+    const parsedInput = Number(input);
+    console.log(`parsedInput: ${typeof parsedInput}`);
+    if (!Number.isInteger(parsedInput)) {
+        alert("that is not a discrete number in the range. TRY AGAIN!");
+        $("#name").val("");
+    }
+    else {
+        guesses.push(parsedInput);
+    }
+    return parsedInput;
+};
 $("#form").on("submit", function (event) {
+    console.log(`randNum: ${randNum}`);
     event.preventDefault();
-    const inputName = $("#name").val();
-    $("#display-text").hide();
-    $("#display-text").text(`Hello, ${inputName}! from a jQuery function ;)`);
-    $("#display-text").fadeIn("slow");
+    const input = $("#name").val();
+    $("#name").text("");
+    const parsedInput = validateInput(input);
+    if (parsedInput === randNum) {
+        $("#result").text(`You guessed the number!`);
+    }
+    else if (parsedInput > randNum) {
+        $("#result").text(`The number is lower than ${parsedInput}`);
+    }
+    else {
+        $("#result").text(`The number is higher than ${parsedInput}`);
+    }
+    if (guesses.length > 0) {
+        $("#guesses").text(`Your guesses: [${guesses.join(", ")}]`);
+    }
 });
 
 
--- src/client/scripts.ts
+++ src/client/scripts.ts
@@ -7,16 +7,38 @@ declare global {
     }
 }
 
+const randNum = Math.floor(Math.random() * 100);
+const guesses: number[] = [];
+
+
+const validateInput = (input: string): number => {
+    const parsedInput = Number(input);
+    console.log(`parsedInput: ${typeof parsedInput}`);
+    if (!Number.isInteger(parsedInput)) {
+        alert("that is not a discrete number in the range. TRY AGAIN!")
+        $("#name").val("");
+    } else { guesses.push(parsedInput); }
+    return parsedInput;
+}
+
 
 $("#form").on("submit", function(event) {
+    console.log(`randNum: ${randNum}`);
     event.preventDefault();
-    const inputName = $("#name").val();
+    const input = $("#name").val() as string;
+    $("#name").text("");
+    const parsedInput = validateInput(input);
 
-    $("#display-text").hide();
-
-    $("#display-text").text(`Hello, ${inputName}! from a jQuery function ;)`)
-    
-    $("#display-text").fadeIn("slow");
+    if (parsedInput === randNum) {
+        $("#result").text(`You guessed the number!`);
+    } else if (parsedInput > randNum) {
+        $("#result").text(`The number is lower than ${parsedInput}`);
+    } else {
+        $("#result").text(`The number is higher than ${parsedInput}`);
+    }
+    if (guesses.length > 0) {
+        $("#guesses").text(`Your guesses: [${guesses.join(", ")}]`);
+    }
 });
 
 export {};
